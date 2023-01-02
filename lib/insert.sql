94734726f7d6ac41bf5de0bca2d6951a96bdf73b
Isabella Costabile <costabileisa@gmail.com>
2023-01-02 11:15:58 -0500

    commit tree 9c7c5df390d42d4864cffc78e4b4dfc2aa1b3ea7

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+("Mr. Chocolate", 10, "M", "Brown", "Friendly", 1),
+("Rowdy", 2, "M", "Red", "Rowdy", 1),
+("Tabitha", 7, "F", "Brown", "Friendly", 1),
+("Sergeant Brown", 5, "M", "Brown", "Aggressive", 1),
+("Melissa", 3, "F", "Black", "Outgoing", 1),
+("Grinch", 9, "M", "Black", "Mean", 1),
+("Wendy", 0, "F", "White", "Playful", 1),
+(NULL, 4, "F", "Brown", "Cheerful", 1);
